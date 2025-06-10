5080ef1e937f1d5753c4d81159f5a8716975e18d
Ryan Irani <ryan.irani@gmail.com>
2025-06-10 23:22:49 +0200

    feat: add subscription page

---
+++ src/components/Subscription.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import Button from "./Button";
+import { useSubscription } from "@/providers/SubscriptionProvider";
+
+const items = [
+  {
+    heading: "Unlock - Places",
+    text: "Remember your trips in 10 picture",
+    svg: "/images/places-black.svg",
+  },
+  {
+    heading: "Unlock - People",
+    text: "Remember your friends and family in only 10 pictures",
+    svg: "/images/people-black.svg",
+  },
+  {
+    heading: "Unlock - Previous months",
+    text: "Organise photos from all previous months.",
+    svg: "/images/date-black.svg",
+  },
+  {
+    heading: "Unlock - Video",
+    text: "Sometimes there is more to remember",
+    svg: "/images/video-black.svg",
+  },
+];
+
+export default function Subscription() {
+  const { isSubOpen, toggleSubscription } = useSubscription();
+  return (
+    <div
+      className={`fixed bottom-0 left-0 right-0 z-50 w-full bg-[var(--brand-bg)] px-6 flex flex-col justify-between transition-transform duration-300 h-[90vh] ${
+        isSubOpen ? "translate-y-0" : "translate-y-full"
+      }`}
+    >
+      <div>
+        <div className="flex justify-between">
+          <h1 className="text-black font-semibold text-[28px] leading-[120%] max-w-[241px]">
+            Subscription
+          </h1>
+          <Image
+            onClick={toggleSubscription}
+            src="/images/X.svg"
+            alt="Cancel Button"
+            width={14}
+            height={14}
+          />
+        </div>
+        <div className="mt-10">
+          {items.map((item, index) => (
+            <div
+              key={index}
+              className="flex flex-row gap-x-5 items-center w-full mt-8"
+            >
+              <Image
+                src={item.svg}
+                alt="Cancel Button"
+                width={20}
+                height={20}
+              />
+
+              <div className="flex flex-col">
+                <h2 className="text-black font-semibold text-lg">
+                  {item.heading}
+                </h2>
+                <p className="text-sm leading-[120%] font-normal text-[#6F6F6F]">
+                  {item.text}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div>
+        <div className="flex flex-row justify-between">
+          <div>
+            <h1 className="text-black font-semibold text-[28px] leading-[120%] max-w-[241px] ">
+              Monthly
+            </h1>
+            <p className="text-lg leading-[120%] font-normal text-[#6F6F6F]">
+              20 kr.
+            </p>
+          </div>
+          <div className="max-w-sm">
+            <Button text="Choose" />
+          </div>
+        </div>
+        <div className="flex flex-row justify-between mt-6">
+          <div>
+            <h1 className="text-black font-semibold text-[28px] leading-[120%] max-w-[241px] ">
+              Yearly
+            </h1>
+            <p className="text-lg leading-[120%] font-normal text-[#6F6F6F]">
+              200 kr.
+            </p>
+          </div>
+          <div className="max-w-sm">
+            <Button text="Choose" />
+          </div>
+        </div>
+        <div className="flex flex-row items-center justify-center gap-1 my-10">
+          <p className="text-black font-semibold text-sm leading-[120%] ">
+            Support
+          </p>{" "}
+          <span
+            className="text-black font-normal italic text-sm"
+            style={{ fontFamily: "var(--font-inria)" }}
+          >
+            indie
+          </span>{" "}
+          <p className="text-black font-semibold text-sm leading-[120%] ">
+            Apps
+          </p>
+        </div>{" "}
+      </div>
+    </div>
+  );
+}
