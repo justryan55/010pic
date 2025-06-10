df77f66e25f700cc1e405c28efbf0c983f9f3464
Ryan Irani <ryan.irani@gmail.com>
2025-06-10 23:23:18 +0200

    feat: add locked item

--- src/app/(app)/people/page.tsx
+++ src/app/(app)/people/page.tsx
@@ -1,11 +1,22 @@
 import AddBtn from "@/components/AddBtn";
+import Image from "next/image";
 import React from "react";
 
 export default function People() {
   return (
-    <div className="flex justify-center items-center min-h-[50vh]">
+    <div className="flex flex-col justify-center items-center min-h-[70vh]">
       <AddBtn />
-      <p>Person</p>
+      <div className="flex flex-row gap-x-2 mt-2">
+        <Image
+          src="/images/lock.svg"
+          width={11}
+          height={15}
+          alt="Image of lock"
+        />
+        <p className="text-sm leading-[120%] font-normal text-[#6F6F6F]">
+          Add Person
+        </p>
+      </div>
     </div>
   );
 }
--- src/app/(app)/places/page.tsx
+++ src/app/(app)/places/page.tsx
@@ -1,5 +1,22 @@
+import AddBtn from "@/components/AddBtn";
+import Image from "next/image";
 import React from "react";
 
 export default function People() {
-  return <div>People</div>;
+  return (
+    <div className="flex flex-col justify-center items-center min-h-[70vh]">
+      <AddBtn />
+      <div className="flex flex-row gap-x-2 mt-2">
+        <Image
+          src="/images/lock.svg"
+          width={11}
+          height={15}
+          alt="Image of lock"
+        />
+        <p className="text-sm leading-[120%] font-normal text-[#6F6F6F]">
+          Add Place
+        </p>
+      </div>
+    </div>
+  );
 }
