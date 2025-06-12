45b4f7c284098f07739e1360ed754e596b59eeda
Ryan Irani <ryan.irani@gmail.com>
2025-06-12 13:08:11 +0200

    feat: allow user to open images in full-screen mode

--- src/components/HomePage/MonthGrid/PhotoGrid.tsx
+++ src/components/HomePage/MonthGrid/PhotoGrid.tsx
@@ -1,45 +1,73 @@
 import { usePhotoFlow } from "@/providers/PhotoFlowProvider";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 interface SelectedImage {
+  id: string;
   src: string;
   name: string;
 }
 
-interface ImageData {
-  src: SelectedImage;
-  name: SelectedImage;
-}
-
 export default function PhotoGrid({ month }: { month: string }) {
-  const { targetYear, imagesByMonth } = usePhotoFlow();
-  const monthKey = `${targetYear}-${month}`;
-  const monthImages: ImageData[] = (imagesByMonth[monthKey] || []).map(
-    (src) => ({ src, name: src })
+  const [fullScreenImage, setFullScreenImage] = useState<SelectedImage | null>(
+    null
   );
 
-  return (
-    <div className="grid grid-cols-5 grid-rows-2 h-56 gap-1 mt-2">
-      {Array.from({ length: 10 }, (_, i) => {
-        const image = monthImages[i];
+  const { targetYear, imagesByMonth } = usePhotoFlow();
+  const monthKey = `${targetYear}-${month}`;
+  const monthImages: SelectedImage[] = imagesByMonth[monthKey] || [];
 
-        return (
-          <div key={i} className="border border-[#DFDFDF] overflow-hidden">
-            {image ? (
-              <Image
-                src={image.src.src}
-                alt={image.name.name}
-                height={50}
-                width={50}
-                className="w-full h-full object-cover"
-              />
-            ) : (
-              <div className="w-full h-full"></div>
-            )}
+  const openFullScreen = (image: SelectedImage) => {
+    setFullScreenImage(image);
+  };
+
+  const closeFullScreen = () => {
+    setFullScreenImage(null);
+  };
+
+  return (
+    <div>
+      <div className="grid grid-cols-5 grid-rows-2 h-56 gap-1 mt-2">
+        {Array.from({ length: 10 }, (_, i) => {
+          const image = monthImages[i];
+
+          return (
+            <div key={i} className="border border-[#DFDFDF] overflow-hidden">
+              {image ? (
+                <Image
+                  src={image.src}
+                  alt={image.name}
+                  height={50}
+                  width={50}
+                  className="w-full h-full object-cover"
+                  onClick={() => openFullScreen(image)}
+                />
+              ) : (
+                <div className="w-full h-full"></div>
+              )}
+            </div>
+          );
+        })}
+      </div>
+
+      {fullScreenImage && (
+        <>
+          <div
+            className={`fixed inset-0 z-50 w-full bg-black/75 px-6 flex flex-col justify-center items-center transition-transform duration-300`}
+            onClick={closeFullScreen}
+          >
+            <Image
+              src={fullScreenImage.src}
+              alt={fullScreenImage.name}
+              width={800}
+              height={600}
+              className="max-w-full max-h-full object-contain"
+              onClick={(e) => e.stopPropagation()}
+              priority
+            />
           </div>
-        );
-      })}
+        </>
+      )}
     </div>
   );
 }
