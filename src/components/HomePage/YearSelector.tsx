dfa83c306faea4b9e683b927167ff7e709698296
Ryan Irani <ryan.irani@gmail.com>
2025-06-12 15:36:04 +0200

    fix: removed absolute positioning

--- src/components/HomePage/YearSelector.tsx
+++ src/components/HomePage/YearSelector.tsx
@@ -29,8 +29,8 @@ export default function YearSelector() {
   const remainingYears = years.filter((year) => !savedYears.includes(year));
 
   return (
-    <div className="relative flex flex-col mt-6 pb-8">
-      <div className=" flex flex-row gap-6 items-center overflow-x-auto max-w-sm [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none] overscroll-x-contain touch-pan-x">
+    <div className="relative flex flex-col max-w-sm ">
+      <div className=" flex flex-row gap-6 mt-6 items-center overflow-x-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none] overscroll-x-contain touch-pan-x">
         {savedYears.map((year) => {
           return (
             <ul key={nanoid()}>
@@ -63,31 +63,32 @@ export default function YearSelector() {
             Add Years
           </p>
         </div>
-        <div
-          className={`absolute min-h-[40px] top-5 flex flex-row gap-7 overflow-x-auto max-w-sm [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none] overscroll-x-contain touch-pan-x transition duration-200 transform ease-out ${
-            isOpen
-              ? "opacity-100 translate-y-0 pointer-events-auto"
-              : "opacity-0 -translate-y-2 pointer-events-none"
-          }`}
-        >
-          {remainingYears.map((year) => (
-            <ul key={nanoid()} className="mt-2 mr-2">
-              <li
-                key={year}
-                onClick={() => addYearToList(year)}
-                className={`hover:cursor-pointer whitespace-nowrap select-none flex-shrink-0 text-black text-sm flex flex-row items-center gap-1`}
-              >
-                <Image
-                  src="/images/plus-black.svg"
-                  width={9}
-                  height={9}
-                  alt="Add year button"
-                />
-                {year}
-              </li>
-            </ul>
-          ))}
-        </div>
+      </div>
+
+      <div
+        className={`flex flex-row gap-7 overflow-x-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none] overscroll-x-contain touch-pan-x transition duration-200 transform ease-out ${
+          isOpen
+            ? "opacity-100 translate-y-0 pointer-events-auto"
+            : "opacity-0 -translate-y-2 pointer-events-none"
+        }`}
+      >
+        {remainingYears.map((year) => (
+          <ul key={nanoid()} className="mt-1 mr-2">
+            <li
+              key={year}
+              onClick={() => addYearToList(year)}
+              className={`hover:cursor-pointer whitespace-nowrap select-none  flex-shrink-0 text-black text-sm flex flex-row items-center gap-1`}
+            >
+              <Image
+                src="/images/plus-black.svg"
+                width={9}
+                height={9}
+                alt="Add year button"
+              />
+              {year}
+            </li>
+          </ul>
+        ))}
       </div>
     </div>
   );
