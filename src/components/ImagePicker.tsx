c3d5646f53b073228a4cf0bbfc8c810a3ff89d7a
Ryan Irani <ryan.irani@gmail.com>
2025-06-14 13:37:07 +0200

    chore: fix typo

--- src/components/ImagePicker.tsx
+++ src/components/ImagePicker.tsx
@@ -143,7 +143,7 @@ const ImagePicker: React.FC<ImagePickerProps> = ({ config }) => {
   return (
     <div
       className={`fixed bottom-0 left-0 right-0 z-50 w-full bg-[var(--brand-bg)] px-6 flex flex-col justify-around transition-transform duration-300 min-h-screen ${
-        isOpen ? "translate-y-0 pointer" : "translate-y-full"
+        isOpen ? "translate-y-0" : "translate-y-full"
       } `}
     >
       <div>
