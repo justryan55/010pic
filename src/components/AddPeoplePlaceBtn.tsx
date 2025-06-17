6fe251cc99da1dd2ae377668643bdf7e693846ea
Ryan Irani <ryan.irani@gmail.com>
2025-06-17 13:12:05 +0200

    fix: update logic to display add people btn

--- src/components/AddPeoplePlaceBtn.tsx
+++ src/components/AddPeoplePlaceBtn.tsx
@@ -5,12 +5,17 @@ import { usePhotoFlow } from "@/providers/PhotoFlowProvider";
 import React from "react";
 
 export default function AddPeoplePlaceBtn() {
-  const { togglePicker, imagesByPlace, targetYear } = usePhotoFlow();
+  const { togglePicker, imagesByPlace, targetYear, imagesByPerson } =
+    usePhotoFlow();
 
   const pathname = useGetPathname();
 
   const placeKeys = Object.keys(imagesByPlace).filter((key) =>
     key.startsWith(`${targetYear}-place-`)
+  );
+
+  const peopleKeys = Object.keys(imagesByPerson).filter((key) =>
+    key.startsWith(`${targetYear}-person-`)
   );
 
   const handleClick = () => {
@@ -24,13 +29,20 @@ export default function AddPeoplePlaceBtn() {
       return;
     }
   };
+
+  const shouldShow =
+    (pathname === "/people" && peopleKeys.length > 0) ||
+    (pathname === "/places" && placeKeys.length > 0);
+
   return (
-    <div onClick={() => handleClick()}>
-      {pathname !== "/date" && placeKeys.length !== 0 && (
-        <p className="text-[13px] leading-[120%] font-normal underline text-left cursor-pointer">
-          Add {pathname === "/places" ? "Place" : "People"}
-        </p>
+    <>
+      {shouldShow && (
+        <div onClick={() => handleClick()}>
+          <p className="text-[13px] leading-[120%] font-normal underline text-left cursor-pointer">
+            Add {pathname === "/places" ? "Place" : "People"}
+          </p>
+        </div>
       )}
-    </div>
+    </>
   );
 }
