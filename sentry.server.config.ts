fda8215c993ac7d653fec58fbe4305d5fd8690a6
Ryan Irani <ryan.irani@gmail.com>
2025-09-23 15:25:44 +0200

    feat: add sentry for remote logging

---
+++ sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://6ac6ce95c85d2c7269a122dbc37cc326@o4510040422285312.ingest.de.sentry.io/4510041799786576",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
